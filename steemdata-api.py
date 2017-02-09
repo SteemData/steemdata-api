e1efc925f48933b95524b80465eb2c801d248cc5
furion <_@furion.me>
2017-02-10 00:24:04 +0100

    first commit

---
+++ steemdata-api.py
@@ -0,0 +1,40 @@
+from flask_api import FlaskAPI
+from flask_api.exceptions import ParseError, NotFound
+from flask_pymongo import PyMongo
+
+app = FlaskAPI(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://steemit:steemit@mongo1.steemdata.com:27017/SteemData'
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def hello_world():
+    return {'success': True}
+
+
+@app.route('/busy/<string:account_name>/with_metadata/<string:following>')
+def busy_account_following(account_name, following):
+    """
+    Fetch users followers or followings and their metadata.\n
+    Usage: `GET /busy/<string:account_name>/with_metadata/<string:following>`\n
+    `following` must be 'following' or 'followers'.\n
+    """
+    if following not in ['following', 'followers']:
+        raise ParseError(detail='following should be following or follower')
+
+    acc = mongo.db['Accounts'].find_one({'name': account_name}, {following: 1, '_id': 0})
+    if not acc:
+        raise NotFound(detail='Could not find STEEM account %s' % account_name)
+
+    allowed_fields = {
+        '_id': 0, 'name': 1, 'sp': 1, 'rep': 1,
+        'followers_count': 1, 'following_count': 1, 'post_count': 1,
+    }
+    accounts_w_meta = mongo.db['Accounts'].find({'name': {'$in': acc[following]}}, allowed_fields)
+    return list(accounts_w_meta)
+
+
+if __name__ == '__main__':
+    app.run()
