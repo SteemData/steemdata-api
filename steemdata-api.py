5cf0dd2da184d98bb47c9ded0cb3003e9281294c
furion <_@furion.me>
2017-02-10 02:50:55 +0100

    [api] busy_account_following sort output by time of following

--- steemdata-api.py
+++ steemdata-api.py
@@ -3,6 +3,8 @@ import os
 from flask_api import FlaskAPI
 from flask_api.exceptions import ParseError, NotFound
 from flask_pymongo import PyMongo
+from funcy import where, first
+from steemdata.helpers import timeit
 
 app = FlaskAPI(__name__)
 
@@ -19,7 +21,8 @@ def hello_world():
 @app.route('/busy.org/<string:account_name>/with_metadata/<string:following>')
 def busy_account_following(account_name, following):
     """
-    Fetch users followers or followings and their metadata.\n
+    Fetch users followers or followings and their metadata.
+    Returned list is ordered by follow time (newest followers first). \n
     Usage: `GET /busy/<string:account_name>/with_metadata/<string:following>`\n
     `following` must be 'following' or 'followers'.\n
     """
@@ -30,12 +33,22 @@ def busy_account_following(account_name, following):
     if not acc:
         raise NotFound(detail='Could not find STEEM account %s' % account_name)
 
+    # if follower list is empty
+    if not acc[following]:
+        return []
+
     allowed_fields = {
         '_id': 0, 'name': 1, 'sp': 1, 'rep': 1,
         'followers_count': 1, 'following_count': 1, 'post_count': 1,
     }
-    accounts_w_meta = mongo.db['Accounts'].find({'name': {'$in': acc[following]}}, allowed_fields)
-    return list(accounts_w_meta)
+    accounts_w_meta = list(mongo.db['Accounts'].find({'name': {'$in': acc[following]}}, allowed_fields))
+
+    # return in LIFO order (last to follow is listed first)
+    accounts_ordered = []
+    with timeit():
+        for a in acc[following]:
+            accounts_ordered.append(first(where(accounts_w_meta, name=a)))
+    return accounts_ordered[::-1]
 
 
 if __name__ == '__main__':
